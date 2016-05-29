09e3b51605f9f2775a4e8da748e8442ab7e8aea3
Klara Köllerström <klarakollerstrom@gmail.com>
2016-05-29 19:12:04 +0200

    Update content in header

--- public/wordpress/wp-content/themes/portfolio/header.php
+++ public/wordpress/wp-content/themes/portfolio/header.php
@@ -33,9 +33,11 @@
 
 					<!-- logo -->
 					<div class="logo">
-						<a href="<?php echo home_url(); ?>">
+						<a class='headlink' href="<?php echo home_url(); ?>">
+							<?php echo bloginfo('name'); ?>
 							<!-- svg logo - toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script -->
-							<img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo" class="logo-img">
+							<!-- <img src="<?php 
+							//echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo" class="logo-img"> -->
 						</a>
 					</div>
 					<!-- /logo -->
