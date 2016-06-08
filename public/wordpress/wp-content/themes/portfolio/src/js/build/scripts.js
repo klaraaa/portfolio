370776da612abd07b49da4bdd7cb5bc0a7723b75
Klara Köllerström <klarakollerstrom@gmail.com>
2016-06-08 14:44:37 +0200

    javascript included and minified following JSCS

---
+++ public/wordpress/wp-content/themes/portfolio/src/js/build/scripts.js
@@ -0,0 +1,28 @@
+  jQuery(document).ready(function($) {
+  console.log('main js inläst');
+  //Desktop view
+  if ($(window).width() >= 1024) {
+    //Göm hamburgerikon
+    $('#menu-icon').hide();
+    //Visa meny
+    $('#main-nav').show();
+  }
+  // Tablet view
+  else if ($(window).width() >= 768) {
+    //Göm hamburgerikon
+    $('#menu-icon').hide();
+    //Visa meny
+    $('#main-nav').show();
+  }
+  //Mobile view
+  else {
+    //Göm meny
+    $('#main-nav').hide();
+    //Lyssna efter click på hamburgarikon
+    $('#menu-icon').on('click', function() {
+      //Toggla fram menyn
+      $('#main-nav').slideToggle();
+    });
+  }
+});
+
